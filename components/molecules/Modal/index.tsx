9cef04bb02eea6dee718345b4638b8ae9801b6a6
Mohamad Farhan <mohamad.farhan@aleph-labs.com>
2023-06-08 04:36:12 +0700

    Add Tutor

--- components/molecules/Modal/index.tsx
+++ components/molecules/Modal/index.tsx
@@ -16,36 +16,34 @@ const ModalDialog = ({
   queue,
 }) => {
   return (
-    <>
-      <Modal show={isShow}>
-        <Modal.Header closeButton onClick={() => hideModal(false)}>
-          <Modal.Title>Form Ubah Jadwal</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div>
-            <label className={className.label}>Pilih Tanggal Service</label>
-            <DatePicker
-              minDate={moment().toDate()}
-              placeholderText="Pilih Tanggal"
-              className="form-select rounded-pill text-lg"
-              selected={formData.startDate}
-              onChange={(date: any) => {
-                onChangeDate(date);
-              }}
-            />
-            <label className="pt-1">{`Jumlah Antrian = ${queue}`}</label>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" onClick={() => hideModal(false)}>
-            Cancel
-          </Button>
-          <Button className="btn btn-sign-up" onClick={onSubmit}>
-            Submit
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+    <Modal show={isShow}>
+      <Modal.Header closeButton onClick={() => hideModal(false)}>
+        <Modal.Title>Form Ubah Jadwal</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div>
+          <label className={className.label}>Pilih Tanggal Service</label>
+          <DatePicker
+            minDate={moment().toDate()}
+            placeholderText="Pilih Tanggal"
+            className="form-select rounded-pill text-lg"
+            selected={formData.startDate}
+            onChange={(date: any) => {
+              onChangeDate(date);
+            }}
+          />
+          <label className="pt-1">{`Jumlah Antrian = ${queue}`}</label>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="danger" onClick={() => hideModal(false)}>
+          Cancel
+        </Button>
+        <Button className="btn btn-sign-up" onClick={onSubmit}>
+          Submit
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 export default ModalDialog;
--- pages/service.tsx
+++ pages/service.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import ServiceForm from "../components/organisms/ServiceForm";
 import jwtDecode from "jwt-decode";
 import { JWTPayloadTypes, UserTypes } from "../services/data-types";
@@ -9,32 +8,85 @@ import {
   getServiceSparepart,
 } from "../services/player";
 import { useRouter } from "next/router";
+import { Button, Modal } from "react-bootstrap";
+import { useEffect, useState } from "react";
 
 export default function Service({ category, spareparts, cars }) {
+  const [showModal, setShowModal] = useState(false);
   const router = useRouter();
   const onService = () => {
     return router.push("/");
   };
+
+  useEffect(() => {
+    setShowModal(true);
+  }, []);
+
+  const modalTutorialCategory = () => {
+    return (
+      <Modal show={showModal}>
+        <Modal.Header closeButton onClick={() => setShowModal(false)}>
+          <Modal.Title>PENTING : Panduan memilih kategori servis</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ul>
+            <li>
+              Perhatikan kategori servis yang tersedia:<b> Servis Berat </b> dan{" "}
+              <b> Servis Ringan. </b>
+            </li>
+            <br />
+            <li>
+              Periksa kebutuhan mobil Anda. Jika Anda hanya membutuhkan servis
+              rutin seperti pergantian oli mesin, oli gardan, dan pemeriksaan
+              kelistrikan, pilihlah kategori servis ringan.
+            </li>
+            <br />
+            <li>
+              Jika Anda membutuhkan servis yang melibatkan pergantian parts
+              besar, seperti komponen mesin utama, sistem suspensi, atau
+              transmisi, pilihlah kategori servis berat.
+            </li>
+          </ul>
+          <p>
+            <b>Peringatan:</b> Harap diperhatikan bahwa bengkel tidak
+            bertanggung jawab atas kesalahan pemilihan kategori servis yang
+            dilakukan oleh pelanggan. Pastikan Anda memilih dengan benar sesuai
+            kebutuhan mobil Anda.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={() => setShowModal(false)}>
+            Tutup
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
+
   return (
-    <section className="sign-up mx-auto pt-lg-100 pb-lg-100 pt-30 pb-47">
-      <div className="container mx-auto">
-        <div className="pb-50">
-          <a href="/" className="text-center" onClick={onService}>
-            <Image
-              alt="logo"
-              src="/icon/construction.png"
-              width={60}
-              height={60}
-            />
-          </a>
+    <>
+      {modalTutorialCategory()}
+      <section className="sign-up mx-auto pt-lg-100 pb-lg-100 pt-30 pb-47">
+        <div className="container mx-auto">
+          <div className="pb-50">
+            <a href="/" className="text-center" onClick={onService}>
+              <Image
+                alt="logo"
+                src="/icon/construction.png"
+                width={60}
+                height={60}
+              />
+            </a>
+          </div>
+          <ServiceForm
+            categoryData={category}
+            sparepartData={spareparts}
+            carsData={cars}
+            setShowModal={setShowModal}
+          />
         </div>
-        <ServiceForm
-          categoryData={category}
-          sparepartData={spareparts}
-          carsData={cars}
-        />
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
